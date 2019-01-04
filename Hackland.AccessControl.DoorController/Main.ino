2a0895208f4c04dd5c49e77275e1fbe52f6bb97e
Gareth Evans <gareth.evans@snipersystems.co.nz>
2019-01-04 16:44:50 +1300

    MFRC522 RFID reader connected as per https://cdn.instructables.com/FZX/IY9U/J48Q0Z0I/FZXIY9UJ48Q0Z0I.LARGE.jpg dumping UID to serial

--- Hackland.AccessControl.DoorController/Main.ino
+++ Hackland.AccessControl.DoorController/Main.ino
@@ -1,7 +1,14 @@
 #include <ESP8266WiFi.h>
+#include <SPI.h>
+#include <MFRC522.h>
 
 const char *ssid = "Hackland";
 const char *password = "hackland1";
+
+#define SS_PIN 4
+#define RST_PIN 5
+
+MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance
 
 // the setup function runs once when you press reset or power the board
 void setup()
@@ -9,26 +16,31 @@ void setup()
   initializeStatusLed();
   initializeSerial();
   initializeWifi();
+  initializeRfidReader();
 }
 void initializeSerial()
 {
   Serial.begin(115200);
-  while (!Serial); // wait for serial attach
+  while (!Serial)
+  {
+    ; // wait for serial attach
+  }
   Serial.println();
   Serial.println();
-  Serial.println("Initializing serial...");
+  Serial.println(F("Initializing serial..."));
 }
 
 void initializeStatusLed()
 {
-  Serial.println("Initializing status...");
+  Serial.println(F("Initializing status..."));
   pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void initializeWifi()
 {
-  Serial.println("Initializing Wifi...");
-  Serial.print("Connecting to ");
+  Serial.println(F("Initializing Wifi..."));
+  Serial.print(F("Connecting to "));
   Serial.print(ssid);
   Serial.print(" ...");
   WiFi.begin(ssid, password); // Connect to the network
@@ -40,15 +52,62 @@ void initializeWifi()
   }
 
   Serial.println();
-  Serial.println("Connection established!");
-  Serial.print("IP: ");
+  Serial.println(F("Connection established!"));
+  Serial.print(F("IP: "));
   Serial.println(WiFi.localIP());
 }
 
+void initializeRfidReader()
+{
+  SPI.begin();                       // Init SPI bus
+  mfrc522.PCD_Init();                // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial(); // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
+}
 void loop()
 {
+  readRfidToSerial();
+  delay(1000);
+}
+
+int attempt = 0;
+void readRfidToSerial()
+{
+  Serial.print(F("Read RFID ("));
+  Serial.print(attempt++);
+  Serial.println(F(")"));
+
+  if (!mfrc522.PICC_IsNewCardPresent())
+  {
+    Serial.println(F("No card present"));
+
+    return;
+  }
+
   digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
   delay(250);                      // wait for a second
   digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
-  delay(250);                      // wait for a second
+
+  // Select one of the cards
+  if (!mfrc522.PICC_ReadCardSerial())
+  {
+    Serial.println(F("Failed to select card"));
+
+    return;
+  }
+
+  Serial.println(F("Card detected"));
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  //mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+
+  Serial.print("UID tag:");
+  String content = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++)
+  {
+    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  Serial.println(content);
 }
