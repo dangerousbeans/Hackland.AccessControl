7cf834ca15f36fea60dcdc53dc8d8b24ba5380de
Gareth Evans <gareth.evans@snipersystems.co.nz>
2019-01-05 16:34:28 +1300

    Blank door mvc

---
+++ ControlPanel/Hackland.AcessControl.Web/Controllers/DoorController.cs
@@ -0,0 +1,17 @@
+﻿using Hackland.AccessControl.Web.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hackland.AccessControl.Web.Controllers
+{
+    [Authorize]
+    public class DoorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
