1c22b2d72e94974f35bb998ea0af8a7766b98f7b
Gareth Evans <gareth.evans@snipersystems.co.nz>
2019-03-10 16:12:01 +1300

    Add static settings model (build time settings, not runtime)

---
+++ ControlPanel/Hackland.AcessControl.Web/ApplicationConfigurationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Hackland.AccessControl
+{
+
+    public class ApplicationConfigurationModel
+    {
+        public bool EnableCreateDefaultUserRoute { get; set; }
+        public string CreateDefaultUserUsername { get; set; }
+        public string CreateDefaultUserPassword { get; set; }
+    }
+}
