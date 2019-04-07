44790e30036b69542e7441856887084fc64f2dff
Gareth Evans <gareth.evans@snipersystems.co.nz>
2019-04-07 12:49:05 +1200

    Change database cases

--- ControlPanel/MigrationScripts/001-InitialSchema.sql
+++ ControlPanel/MigrationScripts/001-InitialSchema.sql
@@ -15,24 +15,24 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-/*LOCK TABLES `usertoken` WRITE , `doorread` WRITE, `persondoor` WRITE, `door` WRITE, `person` WRITE, `roleclaim` WRITE, `userlogin` WRITE, `userrole` WRITE, `role` WRITE, `user` WRITE;*/
+/*LOCK TABLES `UserToken` WRITE , `DoorRead` WRITE, `PersonDoor` WRITE, `Door` WRITE, `Person` WRITE, `RoleClaim` WRITE, `UserLogin` WRITE, `UserRole` WRITE, `Role` WRITE, `User` WRITE;*/
 
-DROP TABLE IF EXISTS `usertoken`;
-DROP TABLE IF EXISTS `doorread`;
-DROP TABLE IF EXISTS `persondoor`;
-DROP TABLE IF EXISTS `door`;
-DROP TABLE IF EXISTS `person`;
+DROP TABLE IF EXISTS `UserToken`;
+DROP TABLE IF EXISTS `DoorRead`;
+DROP TABLE IF EXISTS `PersonDoor`;
+DROP TABLE IF EXISTS `Door`;
+DROP TABLE IF EXISTS `Person`;
 
-DROP TABLE IF EXISTS `roleclaim`;
-DROP TABLE IF EXISTS `userclaim`;
-DROP TABLE IF EXISTS `userlogin`;
-DROP TABLE IF EXISTS `userrole`;
-DROP TABLE IF EXISTS `role`;
-DROP TABLE IF EXISTS `user`;
+DROP TABLE IF EXISTS `RoleClaim`;
+DROP TABLE IF EXISTS `UserClaim`;
+DROP TABLE IF EXISTS `UserLogin`;
+DROP TABLE IF EXISTS `UserRole`;
+DROP TABLE IF EXISTS `Role`;
+DROP TABLE IF EXISTS `User`;
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `role` (
+CREATE TABLE `Role` (
   `Id` int(11) NOT NULL,
   `Name` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `NormalizedName` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -43,30 +43,30 @@ CREATE TABLE `role` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `roleclaim`
+-- Table structure for table `RoleClaim`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `roleclaim` (
+CREATE TABLE `RoleClaim` (
   `Id` int(11) NOT NULL,
   `RoleId` int(11) NOT NULL,
   `ClaimType` longtext COLLATE utf8mb4_unicode_ci,
   `ClaimValue` longtext COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`Id`),
   KEY `IX_RoleClaim_RoleId` (`RoleId`),
-  CONSTRAINT `FK_RoleClaim_Role_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `role` (`Id`) ON DELETE CASCADE
+  CONSTRAINT `FK_RoleClaim_Role_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `Role` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `user`
+-- Table structure for table `User`
 --
 
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `user` (
+CREATE TABLE `User` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `UserName` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `NormalizedUserName` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -91,91 +91,89 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `User`
 --
 
-LOCK TABLES `user` WRITE;
-/*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1, 'agrath@hackland.nz','AGRATH@HACKLAND.NZ',NULL,NULL,'\0','AQAAAAEAACcQAAAAEHqiD8WK3yxG41chU9Im3m/atGwUor471sD2+5s3y0Zw1fXQ+HlJwrvmIzmf325kow==','GTBHYMELIAJYGBFGMDAZ2QS3XCC46A2Y','9fbe66d1-b601-4c8a-ae27-9c7a71d3330d',NULL,'\0','\0',NULL,'',0,NULL,NULL);
-/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+LOCK TABLES `User` WRITE;
+/*!40000 ALTER TABLE `User` DISABLE KEYS */;
+INSERT INTO `User` VALUES (1, 'agrath@hackland.nz','AGRATH@HACKLAND.NZ',NULL,NULL,'\0','AQAAAAEAACcQAAAAEHqiD8WK3yxG41chU9Im3m/atGwUor471sD2+5s3y0Zw1fXQ+HlJwrvmIzmf325kow==','GTBHYMELIAJYGBFGMDAZ2QS3XCC46A2Y','9fbe66d1-b601-4c8a-ae27-9c7a71d3330d',NULL,'\0','\0',NULL,'',0,NULL,NULL);
+/*!40000 ALTER TABLE `User` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `userclaim`
+-- Table structure for table `UserClaim`
 --
-select * from `user`
-
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userclaim` (
+CREATE TABLE `UserClaim` (
   `Id` int(11) NOT NULL,
   `UserId` int(11) NOT NULL,
   `ClaimType` longtext COLLATE utf8mb4_unicode_ci,
   `ClaimValue` longtext COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`Id`),
   KEY `IX_UserClaim_UserId` (`UserId`),
-  CONSTRAINT `FK_UserClaim_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE
+  CONSTRAINT `FK_UserClaim_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `userlogin`
+-- Table structure for table `UserLogin`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userlogin` (
+CREATE TABLE `UserLogin` (
   `LoginProvider` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ProviderKey` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `ProviderDisplayName` longtext COLLATE utf8mb4_unicode_ci,
   `UserId` int(11) NOT NULL,
   PRIMARY KEY (`LoginProvider`,`ProviderKey`),
   KEY `IX_UserLogin_UserId` (`UserId`),
-  CONSTRAINT `FK_UserLogin_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE
+  CONSTRAINT `FK_UserLogin_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `userrole`
+-- Table structure for table `UserRole`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `userrole` (
+CREATE TABLE `UserRole` (
   `UserId` int(11) NOT NULL,
   `RoleId` int(11) NOT NULL,
   PRIMARY KEY (`UserId`,`RoleId`),
   KEY `IX_UserRole_RoleId` (`RoleId`),
-  CONSTRAINT `FK_UserRole_Role_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `role` (`Id`) ON DELETE CASCADE,
-  CONSTRAINT `FK_UserRole_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE
+  CONSTRAINT `FK_UserRole_Role_RoleId` FOREIGN KEY (`RoleId`) REFERENCES `Role` (`Id`) ON DELETE CASCADE,
+  CONSTRAINT `FK_UserRole_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
 --
--- Table structure for table `usertoken`
+-- Table structure for table `UserToken`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `usertoken` (
+CREATE TABLE `UserToken` (
   `UserId` int(11) NOT NULL,
   `LoginProvider` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Name` varchar(128) COLLATE utf8mb4_unicode_ci NOT NULL,
   `Value` longtext COLLATE utf8mb4_unicode_ci,
   PRIMARY KEY (`UserId`),
-  CONSTRAINT `FK_UserToken_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `user` (`Id`) ON DELETE CASCADE
+  CONSTRAINT `FK_UserToken_User_UserId` FOREIGN KEY (`UserId`) REFERENCES `User` (`Id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `door`
+-- Table structure for table `Door`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `door` (
+CREATE TABLE `Door` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `MacAddress` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -191,18 +189,18 @@ CREATE TABLE `door` (
   PRIMARY KEY (`Id`),
   KEY `FK_Door_CreatedUser` (`CreatedByUserId`),
   KEY `FK_Door_UpdatedUser` (`UpdatedByUserId`),
-  CONSTRAINT `FK_Door_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `user` (`Id`),
-  CONSTRAINT `FK_Door_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `user` (`Id`)
+  CONSTRAINT `FK_Door_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `User` (`Id`),
+  CONSTRAINT `FK_Door_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `doorread`
+-- Table structure for table `DoorRead`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `doorread` (
+CREATE TABLE `DoorRead` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `TokenValue` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `Timestamp` datetime(6) DEFAULT NULL,
@@ -212,18 +210,18 @@ CREATE TABLE `doorread` (
   PRIMARY KEY (`Id`),
   KEY `FK_DoorRead_Door` (`DoorId`),
   KEY `FK_DoorRead_Person` (`PersonId`),
-  CONSTRAINT `FK_DoorRead_Door` FOREIGN KEY (`DoorId`) REFERENCES `door` (`Id`),
-  CONSTRAINT `FK_DoorRead_Person` FOREIGN KEY (`PersonId`) REFERENCES `person` (`Id`)
+  CONSTRAINT `FK_DoorRead_Door` FOREIGN KEY (`DoorId`) REFERENCES `Door` (`Id`),
+  CONSTRAINT `FK_DoorRead_Person` FOREIGN KEY (`PersonId`) REFERENCES `Person` (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `person`
+-- Table structure for table `Person`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `person` (
+CREATE TABLE `Person` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Name` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
   `EmailAddress` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -238,18 +236,18 @@ CREATE TABLE `person` (
   PRIMARY KEY (`Id`),
   KEY `FK_Person_CreatedUser` (`CreatedByUserId`),
   KEY `FK_Person_UpdatedUser` (`UpdatedByUserId`),
-  CONSTRAINT `FK_Person_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `user` (`Id`),
-  CONSTRAINT `FK_Person_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `user` (`Id`)
+  CONSTRAINT `FK_Person_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `User` (`Id`),
+  CONSTRAINT `FK_Person_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `persondoor`
+-- Table structure for table `PersonDoor`
 --
 
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `persondoor` (
+CREATE TABLE `PersonDoor` (
   `PersonId` int(11) NOT NULL,
   `DoorId` int(11) NOT NULL,
   `CreatedTimestamp` datetime(6) NOT NULL,
@@ -261,10 +259,10 @@ CREATE TABLE `persondoor` (
   KEY `FK_PersonDoor_CreatedUser` (`CreatedByUserId`),
   KEY `FK_PersonDoor_UpdatedUser` (`UpdatedByUserId`),
   KEY `FK_PersonDoor_Door` (`DoorId`),
-  CONSTRAINT `FK_PersonDoor_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `user` (`Id`),
-  CONSTRAINT `FK_PersonDoor_Door` FOREIGN KEY (`DoorId`) REFERENCES `door` (`Id`),
-  CONSTRAINT `FK_PersonDoor_Person` FOREIGN KEY (`PersonId`) REFERENCES `person` (`Id`),
-  CONSTRAINT `FK_PersonDoor_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `user` (`Id`)
+  CONSTRAINT `FK_PersonDoor_CreatedUser` FOREIGN KEY (`CreatedByUserId`) REFERENCES `User` (`Id`),
+  CONSTRAINT `FK_PersonDoor_Door` FOREIGN KEY (`DoorId`) REFERENCES `Door` (`Id`),
+  CONSTRAINT `FK_PersonDoor_Person` FOREIGN KEY (`PersonId`) REFERENCES `Person` (`Id`),
+  CONSTRAINT `FK_PersonDoor_UpdatedUser` FOREIGN KEY (`UpdatedByUserId`) REFERENCES `User` (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
